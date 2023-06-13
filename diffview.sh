69196ae13f551ce5d01a24e8a670fbc626050d34
Miguel Abad Perez <1781002+tiguelu@users.noreply.github.com>
2023-06-13 16:18:23 +0200

    Adding readme. Syncing diffview script.

--- diffview.sh
+++ diffview.sh
@@ -3,15 +3,17 @@
 readarray -t commits <<< $(git log --reverse --pretty=format:'%h')
 readarray -t messages <<< $(git log --reverse --pretty=format:'%s')
 
-if [ "$1" == "list" ]; then
+if [[ -z "$1" || "$1" == "list" ]]; then
 	for i in "${!commits[@]}"; do
 		echo -e "${commits[$i]}  $i  ${messages[$i]}"
 	done
 	exit 0
 fi
 
+left=$(git ls-tree -d --name-only ${commits[0]} | sort | head -n1)
+right=$(git ls-tree -d --name-only ${commits[0]} | sort | tail -n1)
+
 commit=$1
-[[ -z "$commit" ]] && echo "Missing commit" && exit 1
 [[ $commit == ${commits[0]} || $commit == "0" ]] && echo "First commit, nothing to diff" && exit 1
 
 # Instead of commit, accepts also the position of the commit in the history of changes
@@ -24,7 +26,7 @@ tempdir=$(mktemp -d)
 git worktree add --no-checkout --detach $tempdir
 cd $tempdir
 git diff --name-only "${commit}~..${commit}" | xargs git checkout "${commit}~" --
-mkdir -p $tempdir/{4.11,4.12}
-meld $tempdir/{4.11,4.12}
+mkdir -p $tempdir/{$left,$right}
+meld $tempdir/{$left,$right}
 cd $OLDPWD
 git worktree remove --force $tempdir
